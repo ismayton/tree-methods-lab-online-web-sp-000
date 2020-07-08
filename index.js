0edce0defa491722728feb5168a988967d3b30ca
Ian Mayton <ian.mayton@gmail.com>
2020-07-08 23:42:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,48 @@
+function inOrder(currentNode)
+{
+  if(currentNode.left)
+  {
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if (currentNode.right)
+  {
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(rootNode, newNode)
+{
+  if(rootNode.data == newNode.data) return true;
+  //why is this not short circuting the function to return true if condition is met?
+  
+  if(rootNode.data > newNode.data)
+  {
+    if(rootNode.left)
+    {
+      rootNode = rootNode.left
+      findOrAdd(rootNode, newNode)
+    }
+    rootNode.left = newNode
+  }
+  
+  if(rootNode.data < newNode.data)
+  {
+    if(rootNode.right)
+    {
+      rootNode = rootNode.right
+      findOrAdd(rootNode, newNode)
+    }
+    rootNode.right = newNode
+  }
+}
+
+function max()
+{
+  
+}
+
+function min()
+{
+  
+}
